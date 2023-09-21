4dd7485c34b33064383778b5de41a72b019cc796
Denis <gaz91.91@mail.ru>
2023-09-21 14:21:28 +0500

    Добавляет сборку проекта

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,9 @@
-import mysql, { Connection } from 'mysql2/promise'
+import { createConnection as createMySqlConnection, Connection } from 'mysql2/promise'
 
 export const createConnection = async (): Promise<Connection> => {
-    return await mysql.createConnection({
+    return await createMySqlConnection({
         host: process.env.DB_HOST,
-        port: <number | undefined> process.env.DB_PORT,
+        port: +(process.env.DB_PORT || 3306),
         database: process.env.DB_DATABASE,
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import http from 'http'
+import * as http from 'http'
 import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
