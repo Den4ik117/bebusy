82700df475098595ec3c65c02c6197d3988eecde
Denis Zagvozdin <gaz91.91@mail.ru>
2024-04-25 20:25:18 +0500

    feat: попытка отловить баг на продакшене

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->isProduction()) {
-            $_SERVER['HTTPS'] = 'on';
+            request()->server->set('HTTPS', 'on');
+//            $_SERVER['HTTPS'] = 'on';
         }
     }
 }
